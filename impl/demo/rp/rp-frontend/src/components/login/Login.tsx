7837c01c6a1c640a6b82e20b214783c273fa318e
Albert Solana <iamtxena@gmail.com>
2020-03-13 16:15:02 +0100

    updated frontend

--- impl/demo/rp/rp-frontend/src/components/login/Login.tsx
+++ impl/demo/rp/rp-frontend/src/components/login/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DIDSIOPLoginButton from '../did-siop/LoginButton';
+import { doPostCall } from '../../util/Util';
 
 function Welcome(props: any) {
   return <h1>Hello, {props.name}</h1>;
@@ -9,11 +10,24 @@ function ShowWelcome() {
   return (
     <div>
       <Welcome name="Sara" />
-      <DIDSIOPLoginButton onClick={() => alert("Well done!")}>
-        <span>Login using DID-SIOP</span>
+      <DIDSIOPLoginButton onClick={() => userSignIn()}>
+        <span>DID-SIOP Sign-In</span>
       </DIDSIOPLoginButton>
     </div>
   );
+}
+
+async function userSignIn(): Promise<void> {
+  console.log('Going to Sign-In...')
+
+  const data = { 
+    clientUriRedirect: 'http://localhost:9001/siop/request-urls'
+  };
+  const url = 'http://localhost:9003/siop/user-sessions';
+
+  const response = await doPostCall(data, url)
+
+  console.log('Response:' + response)
 }
 
 export default ShowWelcome;
