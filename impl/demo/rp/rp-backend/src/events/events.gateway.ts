362df762c44ca1c8cbd4faa2ea6dabb69ba36d3f
Albert Solana <iamtxena@gmail.com>
2020-03-20 18:06:48 +0100

    socket.io client/server support

--- impl/demo/rp/rp-backend/src/events/events.gateway.ts
+++ impl/demo/rp/rp-backend/src/events/events.gateway.ts
@@ -2,19 +2,94 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayInit,
+  OnGatewayDisconnect,
   WsResponse,
+  MessageBody,
+  ConnectedSocket,
 } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Server } from 'ws';
+import { Socket, Server } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway(8080)
-export class EventsGateway {
-  @WebSocketServer()
-  server: Server;
+@WebSocketGateway()
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
+  
+  @WebSocketServer() wss: Server;
+
+  private logger: Logger = new Logger('EventsGateway');
+
+  afterInit(server: any) {
+    this.logger.log('Initialized!')
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected:     ${client.id}`)
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected:     ${client.id}`)
+  }
+  
+  @SubscribeMessage('signIn')
+  handleSignInEvent(
+    @MessageBody() message: string,
+    @ConnectedSocket() client: Socket ): WsResponse<unknown> {
+    this.logger.log(`SignIn Received:     ${message}`)
+    return {event: 'signIn', data: `Processing SignIn request for:  ${client.id}`}
+  }
+  
+  @SubscribeMessage('sendSIOPRequestJwtToFrontend')
+  handlePrintQREvent(@MessageBody() message: string): void {
+    this.logger.log(`SIOP Request URI:     ${message}`)
+    this.wss.emit('printQR', message);
+  }
+
+  @SubscribeMessage('sendSignInResponse')
+  handleSignInResponseEvent(@MessageBody() message: string): void {
+    this.logger.log(`SIOP Response Validation:     ${message}`)
+    this.wss.emit('signInResponse', message);
+  }
+/*
+  @SubscribeMessage('events')
+  handleEvent(@ConnectedSocket() client: Socket): WsResponse<unknown> {
+    client.emit('test of emission')
+    const event = 'events';
+    return { event, data };
+  }
+
+  @SubscribeMessage('open')
+  onOpen(@MessageBody() input: unknown): WsResponse<unknown> {
+    console.log(input);
+    const event = 'open';
+    const data = 'Received'
+    return { event, data };
+  }
 
   @SubscribeMessage('events')
+  onEvent(@MessageBody() data: unknown): Observable<WsResponse<number>> {
+    const event = 'events';
+    const response = [1, 2, 3];
+    
+    console.log("Data Received:" + data);
+
+    return from(response).pipe(
+      map(data => ({ event, data })),
+    );
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() input: unknown): WsResponse<unknown> {
+    console.log("Input Received:" + input);
+    const event = 'message';
+    const data = 'Hello client from Server!'
+    return { event, data };
+  }
+  */
+  /*
   onEvent(client: any, data: any): Observable<WsResponse<number>> {
     return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
   }
+  */
+  
 }
--- impl/demo/rp/rp-backend/src/main.ts
+++ impl/demo/rp/rp-backend/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WsAdapter } from '@nestjs/platform-ws';
 import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   const port = 9003
-  const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new WsAdapter(app));
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // app.useWebSocketAdapter(new WsAdapter(app));
+  app.useStaticAssets(join(__dirname, '..', 'static'));
   app.enableCors();
   await app.listen(port);
   Logger.log(`RP Backend server started on http://localhost:${port}`, 'RP Backend server')
