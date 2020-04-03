114aab25ed0b28014b751a59d1d1a7290036cdce
Albert Solana <iamtxena@gmail.com>
2020-04-03 19:35:38 +0200

    updated app name for android

--- impl/demo/siop/mobile-wallet/ReactWallet/android/app/src/main/java/com/reactwallet/MainActivity.java
+++ impl/demo/siop/mobile-wallet/ReactWallet/android/app/src/main/java/com/reactwallet/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ReactWallet";
+    return "siop-react-demo-wallet";
   }
 }
