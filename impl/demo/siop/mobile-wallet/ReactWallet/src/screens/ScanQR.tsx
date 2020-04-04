e9b4b3cff100f08acda40e75e682da2ae61f83b6
Albert Solana <iamtxena@gmail.com>
2020-04-04 17:35:31 +0200

    ScanQR both in iOS and Android

--- impl/demo/siop/mobile-wallet/ReactWallet/src/screens/ScanQR.tsx
+++ impl/demo/siop/mobile-wallet/ReactWallet/src/screens/ScanQR.tsx
@@ -1,38 +1,53 @@
-import React from "react";
+import React from 'react';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text, View } from 'native-base';
 import { ScanQRProps } from "Types";
+import {
+  Linking
+} from 'react-native';
+ 
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
-function ScanQR({ navigation }: ScanQRProps) {
-  return (
-    <Container>
-      <Header />
-      <Content>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Scan a QR</Text>
-        </View>
-      </Content>
-      <Footer>
-        <FooterTab>
-          <Button vertical onPress={() => navigation.navigate('Home')} >
-            <Icon name="home" />
-            <Text>Home</Text>
-          </Button>
-          <Button vertical onPress={() => navigation.navigate('ScanQR')} >
-            <Icon name="camera" />
-            <Text>Scan QR</Text>
-          </Button>
-          <Button vertical onPress={() =>
-              navigation.push('Profile', {
-                name: 'Jane' + Math.floor(Math.random() * 100),
-              })
-            } >
-            <Icon name="person" />
-            <Text>Profile</Text>
-          </Button>
-        </FooterTab>
-      </Footer>
-    </Container>
-  );
+class ScanQR extends React.Component<ScanQRProps> {
+  readonly navigation = (this.props as ScanQRProps).navigation
+
+  onSuccess = (e: { data: string; }) => {
+    console.log(e.data);
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err)
+    );
+  };
+
+  render() {
+    return (
+      <Container>
+        <Content>
+          <QRCodeScanner
+            onRead={this.onSuccess}
+          />
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button vertical onPress={() => this.navigation.navigate('Home')} >
+              <Icon name="home" />
+              <Text>Home</Text>
+            </Button>
+            <Button vertical onPress={() => this.navigation.navigate('ScanQR')} >
+              <Icon name="camera" />
+              <Text>Scan QR</Text>
+            </Button>
+            <Button vertical onPress={() =>
+                this.navigation.push('Profile', {
+                  name: 'Jane' + Math.floor(Math.random() * 100),
+                })
+              } >
+              <Icon name="person" />
+              <Text>Profile</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
 }
 
 export { ScanQR };
